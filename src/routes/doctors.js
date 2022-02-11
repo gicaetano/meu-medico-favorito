c2e92b6723c04cca6d5f8cdf39cc2ee08ea1462f
Gisele Caetano Pereira <87086783+gicaetano@users.noreply.github.com>
2022-02-11 20:45:17 -0300

    :pachage: allRoutes

--- src/routes/doctors.js
+++ src/routes/doctors.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/doctorController");
 
-router.post("/", controller.createDoctor)
-router.get("/", controller.getAllDoctors)
-router.get("/:id", controller.getDoctor)
-router.put("/:id", controller.updateDoctor)
+router.post("/", controller.createDoctor);
+router.get("/", controller.getAllDoctors);
+router.get("/:id", controller.getDoctor);
+router.put("/:id", controller.updateDoctor);
+router.patch("/:id/favorite", controller.updateFavorite);
+router.delete("/:id", controller.deleteDoctor);
 
 module.exports = router;
