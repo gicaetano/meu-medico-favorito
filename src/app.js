04331eba0506709ea85ec361dac84227e7ebbb66
Gisele Caetano Pereira <87086783+gicaetano@users.noreply.github.com>
2022-02-11 22:09:15 -0300

    :up:

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-const doctors = require("./routes/doctors");
-app.use(express.json());
 
+const doctors = require("./routes/doctors");
 const index = require("./routes/index")
+
+app.use(express.json());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -12,6 +13,17 @@ app.use(function(req, res, next) {
     );
     next();
 });
+
+// para que o front consiga utilizar as rotas que vamos criar precisamos dar essa permissão de acesso
+app.options("/*", (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers")
+    res.header(
+      "Access-Control-Allow-Methods",
+      "PUT,POST,GET,DELETE,OPTIONS,PATCH"
+    );
+    res.send("send some thing whatever")
+  })
 
 // ROTAS
 app.use("/", index);
